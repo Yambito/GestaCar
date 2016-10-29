384eb7350d3bb6ac930a065e350c21e814dc854e
Daniel <Daniel@Daniel-PC>
2016-10-29 14:37:44 +0200

    Commit Inicial (Base)

---
+++ GestaCars-persistence/src/main/java/com/kincar/jee/App.java
@@ -0,0 +1,13 @@
+package com.kincar.jee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
